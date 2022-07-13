9443597ae353a646e64257c503c7d0ea2d3f8c8b
Rikimbili <raciel2901@gmail.com>
2022-07-12 23:21:55 -0400

    feat(*): Add incorrect slug redirect and define more colors

---
+++ pages/[...slugs].jsx
@@ -0,0 +1,10 @@
+/*
+  Catch all possible routes and redirect the user to the root slug.
+  Any predefined route takes precedence so this won't override them.
+*/
+import {useRouter} from "next/router";
+
+export default function AnySlug() {
+  const router = useRouter();
+  router.replace("/");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,25 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    colors: {
-      // Need to define a global color scheme for all apps
-      primary: "#C7BCF6",
-      secondary: "#413A7E",
-      background: {
-        dark: "#0D0E16",
+  extend: {
+    theme: {
+      colors: {
+        // Need to define a global color scheme for all apps
+        primary: "#C7BCF6",
+        error: "#FF5B52",
+        warning: "#F1C64D",
+        info: "#95EEF1",
+        success: "#8CEA94",
+        background: {
+          dark: "#0D0E16",
+        },
+        text: {
+          primary: "#F5F3FE",
+          secondary: "#F5F3FEB3", // 70% transparency
+          disabled: "#F5F3FE80", // 50% transparency
+        },
       },
     },
-    extend: {},
   },
   plugins: [],
 };
