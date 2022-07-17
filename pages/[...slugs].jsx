23484cff6b46f2a7e7b2abbd62085421d4074142
Rikimbili <raciel2901@gmail.com>
2022-07-17 01:59:52 -0400

    fix(slugs): Only use router on the client-side

--- pages/[...slugs].jsx
+++ pages/[...slugs].jsx
@@ -3,8 +3,12 @@
   Any predefined route takes precedence so this won't override them.
 */
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function AnySlug() {
   const router = useRouter();
-  router.replace("/");
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
 }
