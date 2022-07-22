078ef232c1443af0ad8106c12d17122e022245c2
Rikimbili <raciel2901@gmail.com>
2022-07-21 23:29:30 -0400

    style(MemberList): Reverse members order

--- components/MembersSection/MemberList.tsx
+++ components/MembersSection/MemberList.tsx
@@ -10,20 +10,22 @@ export default function MemberList({ sortedMembers }: Props) {
   const [dragging, setDragging] = useState(false);
 
   return (
-    <div className="flex flex-row gap-10 flex-wrap justify-center">
-      {sortedMembers.map((member, index) => (
-        <MemberCard
-          key={index}
-          index={index}
-          name={member.name}
-          imageURL={member.imageURL}
-          websiteURL={member.websiteURL}
-          githubURL={member.githubURL}
-          linkedinURL={member.linkedinURL}
-          alreadyDragging={dragging}
-          setAlreadyDragging={setDragging}
-        />
-      ))}
+    <div className="flex flex-wrap-reverse flex-row-reverse gap-10 justify-center">
+      {sortedMembers
+        .map((member, index) => (
+          <MemberCard
+            key={index}
+            index={index}
+            name={member.name}
+            imageURL={member.imageURL}
+            websiteURL={member.websiteURL}
+            githubURL={member.githubURL}
+            linkedinURL={member.linkedinURL}
+            alreadyDragging={dragging}
+            setAlreadyDragging={setDragging}
+          />
+        ))
+        .reverse()}
     </div>
   );
 }
